a01f8e481f4506a69e627a88e11006ef0f2f6f92
prioty23 <hjp0239@gmail.com>
2026-06-10 23:00:53 +0600

    Resolve merge conflicts and fix lint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,10 @@
 import { defineConfig, globalIgnores } from "eslint/config";
 import nextVitals from "eslint-config-next/core-web-vitals";
-<<<<<<< HEAD
 import nextTs from "eslint-config-next/typescript";
 
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
-=======
-
-const eslintConfig = defineConfig([
-  ...nextVitals,
->>>>>>> 37ee4c5f9b12015c115e6a8d8561111b6422890e
   // Override default ignores of eslint-config-next.
   globalIgnores([
     // Default ignores of eslint-config-next:
@@ -18,6 +12,7 @@ const eslintConfig = defineConfig([
     "out/**",
     "build/**",
     "next-env.d.ts",
+    "nexus-bank-website/**",
   ]),
 ]);
 
